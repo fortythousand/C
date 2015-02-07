0b37e9b48c0e3e4c9029de21e8ea7f9ca09bf56d
fortythousand <fortythousand@users.noreply.github.com>
2015-02-07 22:35:12 +0600

    Update massiv.c

--- massiv.c
+++ massiv.c
@@ -5,7 +5,7 @@
 #define false 0
 #define true !false
  
-void main() {
+int main() {
     float a[] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 0.0f};
     float tmp;
     unsigned i, j;
